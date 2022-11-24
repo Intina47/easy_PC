aa19c8f0e0fbc5f9bad6d820731c6f7c03292cc8
mamba <78519682+Intina47@users.noreply.github.com>
2022-11-24 16:06:30 +0000

    lab database population

---
+++ from_lab/addproduct.php
@@ -0,0 +1,15 @@
+<html>
+    <h1>PRODUCT PAGE POPULATOR</h1>
+<form name= "Add new Product" action= "form2.php" method="post">
+Product_ID:<br><input type= "text" name="productID"><br>
+Category:<br><input type= "text" name="Category"><br>
+product_name:<br><input type= "text" name="product_name"><br>
+product_image:<br><input type= "file" name="product_image"><br>
+description:<br><input type= "text" name="description"><br>
+manufacturer:<br><input type= "text" name="manufacturer"><br> 
+retail_price:<br><input type= "text" name="retail_price"><br>
+bulk_price:<br><input type= "text" name="bulk_price"><br>
+
+<br><input type= "submit" name= "submit" value= "submit"/>
+</form>
+</html>
---
+++ from_lab/db.php
@@ -0,0 +1,22 @@
+//http://localhost/database/db.php
+//https://zeno.computing.dundee.ac.uk/2022-ac32006/isiahntina/createtbl.php
+<?php
+echo "Attempting database connection..";
+$host ="silva.computing.dundee.ac.uk";
+$username = "22ac3u10";
+$password = "cba123";
+$database = "22ac3u10";
+
+// Create connection
+try{
+    $mysql = new PDO("mysql:host=".$host.";dbname".$database, $username, $password);
+    echo "Successfully Connected to ".$database, "\n";
+}catch (PDOException $e){
+    echo "Sorry There was an error Executing query\n";
+    echo $e->getMessage();
+}
+
+
+
+// Check connection
+?>

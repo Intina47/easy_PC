22e86abe3928be36a2c471f32ef6ca37f40380e5
mamba <78519682+Intina47@users.noreply.github.com>
2022-11-25 17:20:12 +0000

    current

---
+++ dtb/populator/addproduct.php
@@ -0,0 +1,15 @@
+<html>
+    <h1>PRODUCT PAGE POPULATOR</h1>
+<form name= "Add new Product" action= "form2.php" method="post">
+Product_ID:<br><input required type= "text" name="product_ID"><br>
+Category:<br><input required type= "text" name="category"><br>
+Product_name:<br><input required type= "text" name="product_name"><br>
+Product_image:<br><input required type= "file" name="product_image"><br>
+Product_description:<br><input required type= "text" name="product_description"><br>
+Manufacturer:<br><input required type= "text" name="manufacturer"><br> 
+Retail_price:<br><input required type= "text" name="retail_price"><br>
+Bulk_price:<br><input required type= "text" name="bulk_price"><br>
+
+<br><input type= "submit" name= "submit" value= "submit"/>
+</form>
+</html>
